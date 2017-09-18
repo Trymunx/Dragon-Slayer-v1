5364c393600845b76a5982f421ce8e5bbaf704f3
Trymunx <trymunx@gmail.com>
2017-09-19 00:34:49 +0100

    Add message on starting a fight

--- js/src/GameStates/GS_Fight.js
+++ js/src/GameStates/GS_Fight.js
@@ -190,6 +190,11 @@ GS_Fight.runState = function (GameStateManager) {
     }
   }
 
+  Output.addElement({
+    "entity": "",
+    "content": "You are now fighting a " + Creature.name + "."
+  });
+  
   // Initialise above function
   Input_Text.addEventListener("keydown", fightCommands);
   
