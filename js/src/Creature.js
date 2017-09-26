25c5ca39d201a298c4e2ab0359c9ffdea29a4a9f
Trymunx <trymunx@gmail.com>
2017-09-26 20:58:21 +0100

    Change function to class

--- js/src/Creature.js
+++ js/src/Creature.js
@@ -9,35 +9,29 @@ const CreatureDb = require("../db/Creatures.json");
   * of the setup values (spawnchance, min/maxTotalHP etc.) so they aren't attached.
   * Other than that you can use this identically to the old method.
   */
-function Creature(creatureJson, key) {
-  /* Are these all needed as part of this creature? */
-  this.key = key;
-	this.name = creatureJson["name"];
-	this.namePlural = creatureJson["namePlural"];
-	var jsonAttributes = creatureJson.attributes; //Could use ["attributes"] or just directly access
-	this.attributes = {};
-	/* Does this need to be inside an attributes object? */
-	// Set creature totalHP and currentHP equeal to a random value between min and max HP values.
-	this.attributes.totalHP = this.attributes.currentHP = Math.round(RNG(jsonAttributes.minTotalHP, jsonAttributes.maxTotalHP));
-	this.attributes.aggressive = jsonAttributes.aggressive;
-	this.attributes.healthBar = jsonAttributes.healthBar;
-	/* Don't care about spawnchance etc. so leave it out
-    "attributes": {
-      "maxTotalHP": 50000,
-      "minTotalHP": 15000,
-      "totalHP": 0,
-      "currentHP": 0,
-      "healthBar": "#",
-      "spawnChance": 0.15,
-      "aggressive": true
-    },*/
-	this.attacks = creatureJson.attacks; //Copy entire object, as this is shared between all creatures (of this type)
-  this.drops = creatureJson.drops; //Ditto
-  this.messages = creatureJson.messages; //And again
-	/* You can also declare functions as properties of the creature,
-	 * and they will be per-creature functions
-	 * (Which you probably don't want with the current implementation).
-	 */
+class Creature {
+  constructor(creatureJson, key) {
+    /* Are these all needed as part of this creature? */
+    this.key = key;
+    // this.name = creatureJson["name"];
+    // this.namePlural = creatureJson["namePlural"];
+    var jsonAttributes = creatureJson.attributes; //Could use ["attributes"] or just directly access
+    this.attributes = {};
+    /* Does this need to be inside an attributes object? */
+    // Set creature totalHP and currentHP equeal to a random value between min and max HP values.
+    this.attributes.totalHP = this.attributes.currentHP = Math.round(RNG(jsonAttributes.minTotalHP, jsonAttributes.maxTotalHP));
+    this.attributes.aggressive = jsonAttributes.aggressive;
+    this.attributes.healthBar = jsonAttributes.healthBar;
+    this.attacks = creatureJson.attacks; //Copy entire object, as this is shared between all creatures (of this type)
+    this.drops = creatureJson.drops; //Ditto
+    this.messages = creatureJson.messages; //And again
+  }
+  get name() {
+    return CreatureDb[name].name;
+  }
+  get namePlural() {
+    return CreatureDb[name].namePlural;
+  }
 }
 
 function newCreature (name) {
