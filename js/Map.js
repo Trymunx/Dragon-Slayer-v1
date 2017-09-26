20db677268af73971578f80a94fce1674f1bb55f
Trymunx <8418875+Trymunx@users.noreply.github.com>
2017-09-26 02:28:34 +0100

    Delete Map.js

--- js/Map.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-const drawMap = require("./src/DrawMap.js");
-const genMap = require("./src/GenerateMap.js");
-const RNG = require("./src/utils/RNG.js");
-
-var map = genMap(Math.round(RNG(5,20)));
-drawMap(map);
-// console.log(drawMap(map));
