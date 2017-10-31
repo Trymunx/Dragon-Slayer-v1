098e1917169601af8b30dfb67af62dc557caadd3
Alpvax <alpvax@netscape.net>
2017-10-31 17:07:31 +0000

    Add default GS.name

--- js/src/GameStates/GameState.js
+++ js/src/GameStates/GameState.js
@@ -2,6 +2,7 @@ class GameState {
     constructor (...handledEvents) {
         this.handledEvents = handledEvents != undefined ? handledEvents : [];
         this.player = null;
+        this.name = this.constructor.name + (this.constructor == GameState ? handledEvents : "");
     }
     setPlayer (player) {
         this.player = player;
