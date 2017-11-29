6286f2e8a0fdf9b7a8572937ae326e334f7e09da
Trymunx <trymunx@gmail.com>
2017-11-29 13:14:52 +0000

    Fix variables starting with a number

--- js/src/world/River.js
+++ js/src/world/River.js
@@ -13,7 +13,7 @@ class River extends Terrain {
     let x;
     let firstX;
     let lastX;
-    let 2lastX = this.originX;
+    let SecLastX = this.originX;
     let chunk;
 
     chunk = world.getChunkFromTile(this.originX, this.riverTop);
@@ -23,29 +23,29 @@ class River extends Terrain {
       tile = chunk.getTileFromWorldCoords(x, this.riverTop);
       tile.terrain = "river";
       this.riverTop--;
-      if (lastX === 2lastX) {
+      if (lastX === SecLastX) {
         x = Math.floor(RNG(lastX-1, lastX+1));
       } else {
-        x = Math.floor(RNG(2*lastX-2lastX, lastX));
+        x = Math.floor(RNG(2*lastX-SecLastX, lastX));
       }
-      2lastX = lastX;
+      SecLastX = lastX;
       lastX = x;
     } while (tile.relx >= 0);
 
     x = this.originX;
     lastX = this.originX;
-    2lastX = firstX;
+    SecLastX = firstX;
     chunk = world.getChunkFromTile(this.originX, this.riverBottom);
     do {
       tile = chunk.getTileFromWorldCoords(this.originX, this.riverBottom);
       tile.terrain = "river";
       this.riverBottom++;
-      if (lastX === 2lastX) {
+      if (lastX === SecLastX) {
         x = Math.floor(RNG(lastX-1, lastX+1));
       } else {
-        x = Math.floor(RNG(2*lastX-2lastX, lastX));
+        x = Math.floor(RNG(2*lastX-SecLastX, lastX));
       }
-      2lastX = lastX;
+      SecLastX = lastX;
       lastX = x;
     } while (tile.relx <= chunk.size); // Think this needs to be Chunk.size as size method is a static. Do I need to require Chunk for this?
   }
